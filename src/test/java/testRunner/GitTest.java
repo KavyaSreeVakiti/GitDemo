830af16063441b9cf1e5ab7861f92b5ca06f1a4e
kavyasreevakiti <kavyareddyvakiti@gmail.com>
2022-09-11 16:32:01 +0530

    newbranch2

--- src/test/java/testRunner/GitTest.java
+++ src/test/java/testRunner/GitTest.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 //created new branch
+//created new branch2
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/Features/Git.feature",
